a94e5f640dc87ed7e23bb635a40c24c994f65ee5
Eugene Vedensky <Work@Genyas-MacBook-Air.local>
2017-04-21 15:54:03 -0400

    added authentication via fire base

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,8 @@
+import * as firebase from 'firebase'; 
+export class AuthService{
+
+    signUpUser(email: string, password: string){
+
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => console.log(error))
+    }
+}
--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from '../auth.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -7,7 +8,7 @@ import {NgForm} from '@angular/forms';
 })
 export class SigninComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -16,6 +17,8 @@ onSignUp(form : NgForm){
 
   const email = form.value.email;
   const password = form.value.password;
-
+  console.log(email);
+  console.log(password);
+  this.authService.signUpUser(email, password);
 }
 }
